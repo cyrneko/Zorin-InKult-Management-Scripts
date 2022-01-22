e76fcf92dd210211c4eec6092d20373038a7b189
TGRush <simeonlps@protonmail.com>
2022-01-22 14:17:47 +0100

    lots of work on InKultManagement.sh

--- Rewrite/InKultManagement.sh
+++ Rewrite/InKultManagement.sh
@@ -16,41 +16,80 @@ uline="\e[4m"
 # shellcheck disable=SC2034
 reset="\e[0m"
 
+gui () {
+   echo -e "${green}Starting GUI...${reset}"
+   ask=$(zenity --list --title="Management Options" --column="0" "Restart Pipewire" "Restart PulseAudio (old Audio Server)" "do Accelerated Updates" "Show Help" --width=150 --height=300 --hide-header)
+   if [ "$ask" == "Restart Pipewire" ]; then
+      echo "Restarting Pipewire (Audio Server)..."
+      pipewire
+   elif [ "$ask" == "Restart PulseAudio (old Audio Server)" ]; then
+      echo "Restarting PulseAudio..."
+      pulseaudio
+   elif [ "$ask" == "do Accelerated Updates" ]; then
+      echo "Running apt-fast for accelerated updates...."
+      apt-fast-upgrade
+   elif [ "$ask" == "Show Help" ]; then
+      sleep 1s
+      help
+   fi
+}
+
 pulseaudio() {
-    systemctl --user restart pulseaudio
-    echo -e "${green}PulseAudio restarted!${reset}"
+   systemctl --user restart pulseaudio
+   echo -e "${green}PulseAudio restarted!${reset}"
 }
 
 pipewire() {
-    systemctl --user restart pipewire pipewire-pulse
-    echo -e "${green}pipewire restarted${reset}"
+   systemctl --user restart pipewire pipewire-pulse || echo -e "${red}Restarting Failed!${reset}" || echo -e "${green}pipewire restarted${reset}"
 }
 
-while getopts ":hg" option; do
-   # shellcheck disable=SC2220
-   case $option in
-   h) # display Help
-      echo -e "${red}$(figlet "InKult Setup")"
-      echo "This script sets up Zorin installations at the InKult Youth Center in Germany."
-      echo -e "${reset}------------"
-      echo -e "${blue}${bold}${uline}Syntax${reset}: $0 [Options]"
-      echo ""
-      echo -e "${blue}${bold}${uline}Example options:${reset}"
-      echo -e "$0 -h   |   ${green}shows a help page for this script${reset}"
-      echo -e "$0 -c   |   ${green}starts installation and setup for Zorin 16 Core.${reset}"
-      echo -e "$0 -l   |   ${green}starts installation and setup for Zorin 16 Lite.${reset}"
-      echo -e "$0 -o   |   ${green}attempts to install on other debian-based distributions.${reset}"
-      echo -e "$0 -p   |   ${green}preperation for running the script, not mandatory.${reset}"
-      echo -e "${blue}PLANK=1 can be used to install plank.${reset}"
-      echo "------------"
-      exit
+help() {
+   echo -e "${red}$(figlet "InKult Management")"
+   echo "This script sets up Zorin installations at the InKult Youth Center in Germany."
+   echo -e "${reset}------------"
+   echo -e "${blue}${bold}${uline}Syntax${reset}: $0 [Options]"
+   echo ""
+   echo -e "${blue}${bold}${uline}Example options:${reset}"
+   echo -e "$0 -h   |   ${green}shows a help page for this script${reset}"
+   echo -e "$0 -g   |   ${green}shows a GUI to do different actions${reset}"
+   echo -e "$0 -a   |   ${green}runs apt-fast for accelerated system updates${reset}"
+   echo -e "$0 -pw  |   ${green}restarts PipeWire (Audio Server)${reset}"
+   echo -e "$0 -pa  |   ${green}restarts PulseAudio (Audio Server, default, but older)${reset}"
+   echo "------------"
+   exit
+}
+
+apt-fast-upgrade () {
+   echo -e "${red}starting...${reset}"
+   pkexec apt-fast upgrade -y
+   echo -e "${green}Finished!${reset}"
+}
+
+if [ -n "$1" ]; then
+   case "$1" in
+   -h)
+      help 
       ;;
-   g)
-      echo "test"
+   -pw)
+      echo "Restarting PipeWire..."
+      pipewire
       ;;
-   *) # Invalid option
-      echo "Error: Invalid option"
-      exit
+   -pa)
+      echo "Restarting PulseAudio..."
+      pulseaudio
+      ;;
+   -g)
+      gui
+      ;;
+   -a)
+      echo "Running apt-fast for accelerated updates...."
+      apt-fast-upgrade
+      ;;
+   *)
+      echo "$1 is not an option"
       ;;
    esac
-done
+   shift
+else
+   gui
+fi
